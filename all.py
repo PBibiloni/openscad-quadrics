b2eaa8e33f4d5d73bec1ffa87cfc348fc6ce8cbb
PBibiloni <pedro@bibiloni.es>
2024-05-17 14:20:40 +0200

    Different widths

--- all.py
+++ all.py
@@ -1,10 +1,12 @@
 import cone, ellipsoid, hyperbolicparaboloid, hyperboloid, paraboliccylinder
 
-cone.generate(0.5, 0.5)
-cone.generate(0.5, 1)
-cone.generate(2, 1)
 
-ellipsoid.generate()
-hyperbolicparaboloid.generate()
-hyperboloid.generate()
-paraboliccylinder.generate()
+for w in [1.6, 2, 2.8]:
+    cone.generate(0.5, 0.5, width_mm=w)
+    cone.generate(0.5, 1, width_mm=w)
+    cone.generate(2, 1, width_mm=w)
+
+    ellipsoid.generate(width_mm=w)
+    hyperbolicparaboloid.generate(width_mm=w)
+    hyperboloid.generate(width_mm=w)
+    paraboliccylinder.generate(width_mm=w)
