f30867fb7eaed33c6f5a4ca4bc28197a805bccd7
PBibiloni <pedro@bibiloni.es>
2024-05-17 14:19:11 +0200

    Add to execute all

---
+++ all.py
@@ -0,0 +1,10 @@
+import cone, ellipsoid, hyperbolicparaboloid, hyperboloid, paraboliccylinder
+
+cone.generate(0.5, 0.5)
+cone.generate(0.5, 1)
+cone.generate(2, 1)
+
+ellipsoid.generate()
+hyperbolicparaboloid.generate()
+hyperboloid.generate()
+paraboliccylinder.generate()
