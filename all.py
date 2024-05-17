007c40fe299716d619b18ec81436f9cef42a90d2
PBibiloni <pedro@bibiloni.es>
2024-05-17 20:55:41 +0200

    Include openscad command line tool

--- all.py
+++ all.py
@@ -22,8 +22,8 @@ for path_scad_dir in [p for p in root.iterdir() if p.name.startswith('scad_')]:
     path_stl_dir = root / path_scad_dir.name.replace('scad_', 'stl_')
     os.makedirs(path_stl_dir, exist_ok=True)
     for path_scad_script in [p for p in path_scad_dir.iterdir() if p.suffix == '.scad']:
-        path_stl = path_stl_dir / path_scad_script.name.replace('.scad', '.stl')
-        path_png = path_stl_dir / path_scad_script.name.replace('.scad', '.png')
-        cmd = f'"{OPENSCAD_EXEC}" "{path_scad_script}" --imgsize=1024,1024 --render -D STL_FILE_NAME=\\"{path_stl}\\" -o "{path_png}.png"'
-        print(f'EXECUTING: {cmd}')
-        subprocess.run(cmd, shell=True)
+        for out_ext in ['.stl', '.png']:
+            path_out = path_stl_dir / path_scad_script.name.replace('.scad', out_ext)
+            cmd = f'"{OPENSCAD_EXEC}" "{path_scad_script}" --imgsize=1024,1024 --render -o "{path_out}"'
+            print(f'EXECUTING: {cmd}')
+            subprocess.run(cmd, shell=True)
