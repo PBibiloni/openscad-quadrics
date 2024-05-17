9e9a00831e157ad8bbb9961598403663bbf22aa7
PBibiloni <pedro@bibiloni.es>
2024-05-17 14:53:54 +0200

    Include openscad command line tool

--- all.py
+++ all.py
@@ -6,15 +6,15 @@ import cone, ellipsoid, hyperbolicparaboloid, hyperboloid, paraboliccylinder
 
 OPENSCAD_EXEC = r'C:\Program Files\OpenSCAD\openscad.exe'
 
-# for w in [1.6, 2, 2.8]:
-#     cone.generate(0.5, 0.5, width_mm=w)
-#     cone.generate(0.5, 1, width_mm=w)
-#     cone.generate(2, 1, width_mm=w)
-#
-#     ellipsoid.generate(width_mm=w)
-#     hyperbolicparaboloid.generate(width_mm=w)
-#     hyperboloid.generate(width_mm=w)
-#     paraboliccylinder.generate(width_mm=w)
+for w in [1.6, 2, 2.8]:
+    cone.generate(0.5, 0.5, width_mm=w)
+    cone.generate(0.5, 1, width_mm=w)
+    cone.generate(2, 1, width_mm=w)
+
+    ellipsoid.generate(width_mm=w)
+    hyperbolicparaboloid.generate(width_mm=w)
+    hyperboloid.generate(width_mm=w)
+    paraboliccylinder.generate(width_mm=w)
 
 
 root = Path(__file__).parent
