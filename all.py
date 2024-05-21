4e8609e4dc5b1a661ae3c8d9a646285d21e71d05
PBibiloni <pedro@bibiloni.es>
2024-05-22 00:03:18 +0200

    Update models

--- all.py
+++ all.py
@@ -9,7 +9,7 @@ OPENSCAD_EXEC = r'C:\Program Files\OpenSCAD\openscad.exe'
 for w in [1.6, 2, 2.8]:
     cone.generate(0.5, 0.5, width_mm=w)
     cone.generate(0.5, 1, width_mm=w)
-    cone.generate(2, 1, width_mm=w)
+    cone.generate(2, 2, width_mm=w)
 
     ellipsoid.generate(width_mm=w)
     hyperbolicparaboloid.generate(width_mm=w)
